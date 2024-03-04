703e2896e802fa8ed83292f6c803d860b86b324c
Doidoking <minesottile@gmail.com>
2024-03-04 17:51:32 -0300

    Atualizando aulas

--- Aula 23_Exercicio_Global/assets/js/main.js
+++ Aula 23_Exercicio_Global/assets/js/main.js
@@ -4,20 +4,9 @@ const imcResultado = document.querySelector('.resultado');
 imcForm.addEventListener('submit', function (segurEvento) {
     segurEvento.preventDefault();
     console.log('Evento Prevenido');
-    setResultado('Salve testes');
     imcCalculo();
 
 });
-function setResultado(msg) {
-    
-    imcResultado.innerHTML = '';
-    const p = document.createElement('p');
-    p.classList.add('paragrafo-resultado');
-    p.innerHTML = 'Deu certo';
-    imcResultado.appendChild(p);
-
-};
-
 
 function imcCalculo() {
     const pesoIMC = imcForm.querySelector('.peso');
@@ -25,7 +14,18 @@ function imcCalculo() {
 
     const peso = Number(pesoIMC.value);
     const altura = Number(alturaIMC.value);
-    const imcContas = (peso / (altura * altura)).toFixed(1);
+    const imc = getImc(peso,altura);
+    const grauImc = getGrauIMC(imc);
+    const msg = `Seu imc é ${imc} ${grauImc}`;
+
+    setResultado(msg, true);
+
+    
+    function getImc(peso, altura){
+        const imc = peso / altura**2;
+        return imc.toFixed(1)
+    }
+
     if (!peso) {
         setResultado('Peso Inválido', false)
         return;
@@ -35,27 +35,50 @@ function imcCalculo() {
         return;
     }
 
-    if (imcContas <= 18.5) {
-        imcResultado.innerHTML += `<p> ${imcContas} Abaixo do Peso</p>`;
-    }
-    else if (imcContas > 18.5 || imcContas < + 24.9) {
-        imcResultado.innerHTML += `<p> ${imcContas} Peso normal</p>`;
-    }
-    else if (imcContas >= 25 || imcContas <= 29.9) {
-        imcResultado.innerHTML += `<p> ${imcContas} Sobrepeso</p>`;
-    }
-    else if (imcContas >= 30 || imcContas <= 34.9) {
-        imcResultado.innerHTML += `<p> ${imcContas} Obesidade Grau 1</p>`;
-    } else if (imcContas >= 35 || imcContas <= 39.9) {
-        imcResultado.innerHTML += `<p> ${imcContas} Obesidade Grau 2</p>`;
-    }
-    else if (imcContas > 40) {
-        imcResultado.innerHTML += `<p> ${imcContas} obesidade Grau 3</p>`;
-    }
-    else {
-        console.log("Tente novamente");
-        imcResultado.innerHTML += `<p> Tente novamente </p>`;
-    }
-    console.log(peso, altura, imcContas);
 
+    function getGrauIMC(imc){
+
+        const grau =['Abaixo do Peso', 'Peso normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'obesidade Grau 3']
+
+        if (imc >= 39.9) return grau[5];
+        
+         if (imc >= 34.9) return grau[4];
+        
+         if (imc >= 29.9)  return grau[3];
+        
+         if (imc >= 24.9) return grau[2];
+        
+         if (imc >=18.5) return grau[1];
+         
+         if (imc<18.5) return grau[0];
+
+        else if (imc > 18.5 || imc <= 24.9) {
+            imcResultado.innerHTML += `<p> ${imc} Peso normal</p>`;
+        }
+        else if (imc >= 25 || imc <= 29.9) {
+            imcResultado.innerHTML += `<p> ${imc} Sobrepeso</p>`;
+        }
+        else if (imc >= 30 || imc <= 34.9) {
+            imcResultado.innerHTML += `<p> ${imc} Obesidade Grau 1</p>`;
+        } 
+        else if (imc >= 35 || imc <= 39.9) {
+            imcResultado.innerHTML += `<p> ${imc} Obesidade Grau 2</p>`;
+        }
+        else if (imc > 40) {
+            imcResultado.innerHTML += `<p> ${imc} obesidade Grau 3</p>`;
+        }
+        else {
+            console.log("Tente novamente");
+        
+        }
+        console.log(peso, altura, imc);
+        };
+};
+function setResultado(msg, isValid) {    
+    imcResultado.innerHTML = '';
+    
+    const p = document.createElement('p');
+    if (isValid) p.classList.add('paragrafo-resultado');
+    p.innerHTML = msg;
+    imcResultado.appendChild(p);
 };
