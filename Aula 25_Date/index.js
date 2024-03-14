44b5b106e5672cee57f7f6c381ae69cec9242c88
Doidoking <minesottile@gmail.com>
2024-03-14 18:13:12 -0300

    Inicio aula 25

---
+++ Aula 25_Date/index.js
@@ -0,0 +1,9 @@
+/*
+const horaEspecifica = 60 * 60 * 3 *1000; Aqui criamos o contador de 3 horas
+const dias = 60 * 60 * 24 * 1000; Neste comando adicionamos o contador de 24 horas
+const data = new Date(0); Comando para informar a data de marco 0
+const dataCrua = new Date();  Ele ira pegar a data atual exata
+*/
+
+const dataEspecifica = new Date(2015, )
+console.log(data.toString());
